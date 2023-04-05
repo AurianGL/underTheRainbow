bd77093bb8a2b4b0a0a0eb0fc95f82f178877121
Aurian Guerard <aurian.guerard@pennylane.com>
2023-04-05 18:09:31 +0200

    the sunset of civilisation

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require "io/console"
 require "./services/select.rb"
 require "./display.rb"
+require "./tree.rb"
+require 'csv'
+
 
 Dir.glob(File.join(File.dirname(__FILE__), '**', 'init.rb')).each do |file|
   puts file
@@ -11,19 +14,63 @@ class App
   def initialize
     @selected_item_index = 0
     @menu_items = [
-      "1. Hello world",
-      "2. go fuck yourself"
+      "1. Go to Berlin",
+      "2. NØ"
     ]
     @actions = [:merme, :radar]
+    @choices = []
   end
 
   def launch
     puts "\nWelcome to the Magical World of OZ"
     puts "do you want to dive in the rabbit hole ?"
     answer = gets.chomp
-    answer == "yes" ? selection = MenuSelect.call(@menu_items).selection : exit
-    send(@actions[selection])
-    exit
+    if answer == "yes"
+      navigate_decision_tree(TREE)
+    else
+      exit
+    end
+  end
+
+  private
+
+  def navigate_decision_tree(tree)
+    @current_node = tree
+    loop do
+      if @current_node.is_a?(Symbol)
+        send(@current_node)
+        break
+      else
+        @selection = MenuSelect.call(@current_node.keys.flatten).selection
+        @previous_node = @current_node
+        next_node_key = @current_node.keys[@selection]
+        @current_node = @current_node[next_node_key]
+      end
+    end
+  end
+
+  def store_date
+  end
+
+  def display_choices
+    puts @choices.flatten
+  end
+
+  def back(field = nil)
+    @choices << "#{field} : #{@previous_node.keys[@selection]}" if field
+    navigate_decision_tree(TREE["1. Go to Berlin"])
+  end
+
+  def date
+    back("date")
+  end
+
+  def act
+    back("activité")
+  end
+
+  def place
+    back("lieu")
   end
 
   def merme
@@ -34,10 +81,26 @@ class App
         char_to_trim = (line.length - 100) / 2
         line.slice(/^.{#{char_to_trim}}(.{0,#{line.length-(char_to_trim * 2)}}).{#{char_to_trim}}$/, 1) 
       end
-
       # Print the trimmed lines to the console
       puts trimmed_lines
     end
+  end
+
+  def suggestions
+    puts "type in your suggestions"
+    suggestion = gets.chomp
+    @choices << "suggestions : #{suggestion}"
+    back 
+  end
+
+  def stop    
+    File.open("choices.txt", "w") do |file|
+      @choices.each do |choice|
+        file.puts choice
+      end
+    end
+    puts "Thanks for your choices they have been saved in choices.csv"
+    exit
   end
 
   def radar
---
+++ tree.rb
@@ -0,0 +1,29 @@
+# Purpose: To store the tree data structure to plan a trip to Berlin
+# each item represnent a set of choices which each are assiciated with a symbol
+
+TREE = {
+    "1. Go to Berlin" => {
+        "1.1. What is the date of your trip ?" => {
+            "1. 6-7-8 mai" => :date,
+            "2. 13-14 mai" => :date,
+            "3. 18-19-20-21 mai" => :date,
+            "4. 27 mai and beyond" => :date,
+            '5. I don\'t know yet' => :date,
+            '6. pas en mai' => :date
+        },
+        "What type of activities do you want to do ?" => {
+            "1. Eat, Pray, Love" => :act,
+            "2. 48h berghain" => :act,
+            "3. I don't know, I'll just follow" => :act,
+        } ,
+        "What type of places do you want to visit ?" => {
+            "1. Museum" => :place,
+            "2. Bars" => :place,
+            "3. Clubs" => :place,
+            "4. I don't know, I'll just follow" => :place,
+        },
+        "make suggestions" => :suggestions,
+        "Stop that monstruosity" => :stop
+    },
+    "2. NØ" => :exit
+}
